967238782ea8aec0d3d89e9ff04ed12a31f4fa11
5 <5>
2022-05-07 06:16:36 +0800

    Build second test (red light) - game.ts refactor

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -8,7 +8,7 @@ describe('Game', () => {
   // 建立第一個測試 (紅燈): 第一集買 1 本 總價應為 100 元
   test('Buy 1 book for the first episode', () => {
     const game = new Game();
-    game.addBook(1);
+    game.addBook(1, 1);
     
     expect(game.getTotalPrice).toBe(100);
   });
--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,7 +1,30 @@
 export class Game {
-    addBook(pins: number) {}
+    private bookList: number[] = [];
+    private totalPrice = 0;
+
+    private catalogue = new Map([
+        [1, 0],
+        [2, 0.05],
+        [3, 0.1],
+        [4, 0.2],
+        [5, 0.25],
+    ]); 
+
+    addBook(bookId: number, bookNum: number) {
+        this.bookList[bookId++] = bookNum;
+    }
 
     get getTotalPrice() {
-        return 100;
+        let discount = this.bookList.filter(item => item != 0).length;
+        if (discount > 5) discount = 5;
+        let mapValue = this.catalogue.get(discount);
+        if (mapValue !== undefined) {
+            return this.bookList.reduce(x => x) * (1 - mapValue);
+        } 
+        else {
+            return this.bookList.reduce(x => x);
+        }
     }
+
+
 }
