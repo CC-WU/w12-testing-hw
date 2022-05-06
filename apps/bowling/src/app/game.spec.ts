18365df17c42d151030862c09d0be5e843c92d93
5 <5>
2022-05-07 07:39:58 +0800

    Build fifth test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -12,24 +12,34 @@ describe('Game', () => {
   });
 
   // 建立第一個測試 (紅燈): 第一集買 1 本 總價應為 100 元
-  test('Buy 1 book for the first episode', () => {
+  test('1 copies of the first book', () => {
     game.addBook(1, 1);
     
     expect(game.getTotalPrice).toBe(100);
   });
 
-  test('Buy 1 for the first episode, 1 for the second episode', () => {
+  test('1 copies of the first book and 1 copies of the second book', () => {
     game.addBook(1, 1);
     game.addBook(2, 1);
     
     expect(game.getTotalPrice).toBe(190);
   });
 
-  test('Buy 1 for the first episode, 2 for the second episode', () => {
+  test('1 copies of the first book and 2 copies of the second book', () => {
     game.addBook(1, 1);
     game.addBook(2, 2);
     
     expect(game.getTotalPrice).toBe(290);
+  });
+
+  test('2 copies of the first book, 2 copies of the second book, 2 copies of the third book, 1 copy of the fourth book and 1 copy of the fifth book', () => {
+    game.addBook(1, 2);
+    game.addBook(2, 2);
+    game.addBook(3, 2);
+    game.addBook(4, 1);
+    game.addBook(5, 1);
+    
+    expect(game.getTotalPrice).toBe(675);
   });
 
 
